9cd046e02176b0b17f3a31179068587bd2e02dc1
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2025-01-17 21:05:58 +0530

    fixed

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -70,10 +70,10 @@ export function NavUser({ user }) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={userLogo} alt={user.name} />
+                  <AvatarImage src={userLogo} alt={"user.name"} />
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate font-semibold">{"user.name"}</span>
                   <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
