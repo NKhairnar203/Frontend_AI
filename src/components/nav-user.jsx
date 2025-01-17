50562378e20578f5257f3817478e0f94f525080c
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2025-01-17 21:08:55 +0530

    fixed

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -74,7 +74,7 @@ export function NavUser({ user }) {
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{"user.name"}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                  <span className="truncate text-xs">{"user.email"}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
