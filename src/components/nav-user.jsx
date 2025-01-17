5d78a06629d477566296bd80c3d8210c1ccb35ff
Nilesh Khairnar <139966220+NKhairnar203@users.noreply.github.com>
2025-01-17 22:58:31 +0530

    Backend add

--- src/components/nav-user.jsx
+++ src/components/nav-user.jsx
@@ -25,7 +25,7 @@ import { toast } from "react-toastify";
 
 export function NavUser({ user }) {
   const { isMobile } = useSidebar();
-
+  console.log(user.email)
   const handleLogout = () => {
     axiosInstance
       .get("/user/logout")
@@ -55,7 +55,7 @@ export function NavUser({ user }) {
                 <AvatarImage src={userLogo} alt={"userlogo"} />
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{"user.name"}</span>
+                <span className="truncate font-semibold">{user.name}</span>
                 <span className="truncate text-xs">Profile</span>
               </div>
               <ChevronsUpDown className="ml-auto size-4" />
@@ -70,11 +70,11 @@ export function NavUser({ user }) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={userLogo} alt={"user.name"} />
+                  <AvatarImage src={userLogo} alt={user.name} />
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{"user.name"}</span>
-                  <span className="truncate text-xs">{"user.email"}</span>
+                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
